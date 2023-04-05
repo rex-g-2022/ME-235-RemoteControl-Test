77278281e50abc987e5380701d001e93fc14cf23
rex-g-2022 <113227615+rex-g-2022@users.noreply.github.com>
2023-04-04 18:25:56 -0700

    Add files via upload

---
+++ espNowReceiver/src/main.cpp
@@ -0,0 +1,67 @@
+#include <Arduino.h>
+#include <esp_now.h>
+#include <WiFi.h>
+
+int yaw;
+int throttle;
+int pitch;
+int roll;
+
+// Structure example to receive data
+// Must match the sender structure
+typedef struct struct_message {
+  /*    char a[32];
+      int b;
+      float c;
+      bool d;*/
+  int a;
+  int b;
+  int c;
+  int d;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  yaw = myData.a;
+  throttle = myData.b;
+  roll = myData.c;
+  pitch = myData.d;  
+  Serial.print("yaw: ");
+  Serial.println(myData.a);
+  Serial.print("throttle: ");
+  Serial.println(myData.b);
+  Serial.print("roll: ");
+  Serial.println(myData.c);
+  Serial.print("pitch: ");
+  Serial.println(myData.d);
+  Serial.println();
+  
+}
+
+void setup() {
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Once ESPNow is successfully Init, we will register for recv CB to
+  // get recv packer info
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void loop() {
+
+}
